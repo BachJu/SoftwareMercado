d4896caca208385dfc8bb82b5d2142c86a3fadd1
Juliana Bach <bachjuliana.julianasbach@gmail.com>
2024-02-11 17:01:43 -0400

    Segunda versão

--- produtos.c
+++ produtos.c
@@ -5,24 +5,26 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+//Cadastra um novo produto
 void novoProduto(){
+    //Limpa a tela
     limparTela();
+
+    //Separador estético
     separador();
 
-    char setHig[10], setLimp[10], setBeb[10], setFrio[5], setPad[10], setAco[10];
+    //Declara variáveis de contole para comparar com o que o usuário digitar
     bool hig = false, limp = false, beb = false, frio = false, pad = false, aco = false;
-    
-    strcpy(setHig, "Higiene");
-    strcpy(setLimp, "Limpeza");
-    strcpy(setBeb, "Bebidas");
-    strcpy(setFrio, "Frios");
-    strcpy(setPad, "Padaria");
-    strcpy(setAco, "Acougue");
 
+    //Declara variável do tipo "Produtos"
     Produtos novo;
     
+    //Pede o identificador do novo produto que será cadastrado
     printf("Digite o codigo do produto: ");
     scanf(" %d", &novo.identificador);
+
+    //Verifica se é um identificador válido
     while (novo.identificador < 0)
     {
         printf("Esse valor nao e um numero valido\n");
@@ -30,32 +32,34 @@ void novoProduto(){
         scanf(" %d", &novo.identificador);
     }
     
-
+    //Pede o setor do novo produto
     printf("Digite o setor do produto: ");
+
+    //Verifica se é um setor válido
     scanf(" %[^\n]s", novo.setor);
     while (!(hig || limp || beb || frio || pad || aco))
     {
-        if (strcmp(novo.setor, setHig) == 0)
+        if (strcmp(novo.setor, "Higiene") == 0)
         {
             hig = true;
         }
-        else if (strcmp(novo.setor, setLimp) == 0)
+        else if (strcmp(novo.setor, "Limpeza") == 0)
         {
             limp = true;
         }
-        else if (strcmp(novo.setor, setBeb) == 0)
+        else if (strcmp(novo.setor, "Bebida") == 0)
         {
             beb = true;
         }
-        else if (strcmp(novo.setor, setFrio) == 0)
+        else if (strcmp(novo.setor, "Frios") == 0)
         {
             frio = true;
         }
-        else if (strcmp(novo.setor, setPad) == 0)
+        else if (strcmp(novo.setor, "Padaria") == 0)
         {
             pad = true;
         }
-        else if (strcmp(novo.setor, setAco) == 0)
+        else if (strcmp(novo.setor, "Acougue") == 0)
         {
             aco = true;
         }
@@ -76,12 +80,16 @@ void novoProduto(){
         
         
     }
+
+    //Pede o nome do produto
     printf("Digite o nome do produto: ");
     scanf(" %[^\n]s", novo.nomeProd);
 
+    //Pede o preço do produto
     printf("Digite o preço do produto: ");
     scanf(" %lf", &novo.preco);
 
+    //Pede a data de validade
     printf("Digite a data de validade.\n");
     printf("Dia: ");
     scanf(" %d", &novo.validade.dia);
@@ -92,14 +100,18 @@ void novoProduto(){
     printf("Ano: ");
     scanf(" %d", &novo.validade.ano);
 
+    //Pede a quantidade armazenada no estoque do novo produto
     printf("Digite a quantidade no estoque: ");
     scanf(" %d", &novo.estoque);
+
+    //Verifica se é um valor válido
     if (novo.estoque < 0)
     {
         printf("Quantidade invalida.\n");
         printf("Digite uma quantidade valida\n");
         scanf(" %d", &novo.estoque);
     }
-    separador();
+
+    //Limpa tela
     limparTela();
 }
